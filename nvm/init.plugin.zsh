68e3e21fc8fd99257bb98978c491db235f03abc2
Brian Devins-Suresh <brian.devinssuresh@datadoghq.com>
2020-05-12 10:55:13 -0400

    lazy load nvm

--- nvm/init.plugin.zsh
+++ nvm/init.plugin.zsh
@@ -1,5 +1,9 @@
-# Adapted from: https://www.reddit.com/r/node/comments/4tg5jg/lazy_load_nvm_for_faster_shell_start/d5ib9fs
+# https://gist.github.com/gfguthrie/9f9e3908745694c81330c01111a9d642
 
 export NVM_DIR="${HOME}/.local/opt/nvm"
-echo "Loading NVM..."
-[[ -s "${NVM_DIR}/nvm.sh" ]] && . "${NVM_DIR}/nvm.sh"
+[ -s "${NVM_DIR}/bash_completion" ] && . "${NVM_DIR}/bash_completion"  # This loads nvm bash_completion
+
+# add our default nvm node (`nvm alias default 10.16.0`) to path without loading nvm
+export PATH="$NVM_DIR/versions/node/v$(<$NVM_DIR/alias/default)/bin:$PATH"
+# alias `nvm` to this one liner lazy load of the normal nvm script
+alias nvm="unalias nvm; [ -s "${NVM_DIR}/nvm.sh" ] && . "${NVM_DIR}/nvm.sh"; nvm $@"
